f7ed939862c41401f07656c020c43877632bb5dc
PHFernandes9 <paulohenriquefernandes3008@gmail.com>
2025-03-30 10:39:49 -0300

    first commit

--- main.py
+++ main.py
@@ -4,63 +4,85 @@ import requests
 from bs4 import BeautifulSoup
 import time
 from datetime import datetime
+import sqlite3
 
 
 def buscalink():
-    url="https://www.mercadolivre.com.br/console-sony-playstation-5-digital-slim-1tb-branco-jogos-returnal-e-ratchet-clank-controle-sem-fio-dualsense-branco/p/MLB37494438#reco_item_pos=1&reco_backend=item_decorator&reco_backend_type=function&reco_client=home_items-decorator-legacy&reco_id=5570092a-48f1-4881-b488-e4ace576e0b9&reco_model=&c_id=/home/navigation-trends-recommendations/element&c_uid=cc2ed833-602e-40f2-9ab5-602d1705ab42&da_id=navigation_trend&da_position=1&id_origin=/home/dynamic_access&da_sort_algorithm=ranker"
-    response=requests.get(url)
+    url = "https://lista.mercadolivre.com.br/play5#D[A:play5,L:undefined,MLB1743]"
+    response = requests.get(url)
     return response.text
 
-def pase_page (html):
-    soup=BeautifulSoup(html,'html.parser')
-    nome=soup.find('h1',class_='ui-pdp-title').get_text(strip=True)
-    price =soup.find('span',class_='andes-money-amount__fraction').get_text(strip=True)
+def parse_page(html):
+    soup = BeautifulSoup(html, 'html.parser')
 
-    return {'Nome' : nome,
-            'Preço':price,
-            'Timestamp': datetime.now().strftime("%Y-%M-D")
-            }
+    # Encontrando todos os nomes dos produtos
+    produtos = soup.find_all('div', class_='ui-search-result__wrapper')
+    lista = []
+    # Criando lista de nomes
+    for produto in produtos:
+
+        nome = produto.find('h3',class_='poly-component__title-wrapper')
+        price = produto.find('span',class_='andes-money-amount andes-money-amount--cents-superscript')
+        price=price.text.strip()
+        lista.append({'Nome' : nome.text.strip(),'Valor':int(price.replace('R$','').replace('.','')) ,'Timestamp' : datetime.now().strftime("%Y-%m-%d %H:%M")})
+    
+    return lista 
+    # Criando lista de nomes
 
 
-def create_db(db_name='price_ps4.db'):
-    conn = sqlite3.connect(db_name)
+#cria/ connecta o banco de dados e retorna a conxao
+def cria_banco(db='PS4.db'):
+    conn = sqlite3.connect(db)
     return conn
 
 
-def criar_tabela(conn):
-
-    cursor=conn.cursor()
-    cursor.execute (
-
-        '''
-        CREATE TABLE IF NOT EXISTS PRICE  (
-        
-        ID PRIMARY KEY, 
-        NOME TEXT,
-        PRICE INTEGER,
-        TIMESTAMP TEXT
-        )  
-     '''
-    ) 
+def cria_tabela(conn):
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Nome TEXT,
+            Valor REAL,
+            timestamp TEXT
+        )
+    ''')
     conn.commit()
 
-def save_to_db(conn,data):
-    df = pd.DataFrame([data])
-    df.to_sql('prices', conn, if_exists='append', index=False)
+def save_to_datetme(conn,data):
+    df = pd.DataFrame(data)
+    df.to_sql('tes',conn ,if_exists= 'append',index=False)
+
+
+
+def consulta(conn):
+    cursor=conn.cursor()
+    cursor.execute ('SELECT * FROM  tes  ORDER BY   Nome')
+
+    executar=cursor.fetchall()
+    df=pd.DataFrame(executar)
+    return df
 
 
 if __name__=="__main__":
-    
-    conn=create_db()
-    criar_tabela(conn)
+
+    # Obtendo o HTML da página
+    banco_de_dados = cria_banco()
+    Tabela = cria_tabela(banco_de_dados)
+
+    print(consulta(banco_de_dados))
+
+    # while True:
+    # #Extraindo os nomes dos produtos
+    #     html = buscalink()
+    #     dados = parse_page(html)
+        
+    #     save_to_datetme(banco_de_dados,dados)
+    #     print('dados inseridos')
 
 
-    while True:
+    # #Criando um DataFrame com os resultados
 
-        dados=buscalink()
-        product_info=pase_page(dados)
-        save_to_db(conn,product_info)
-        print ('Dados salvos')
-        time.sleep(10)
+    # banco_de_dados.close()
 
-    conn.close
+    # #Exibindo os dados
+
